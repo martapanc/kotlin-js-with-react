9efc8b02717b03b44700ff42f31178fe2365b2b8
Marta Pancaldi <marta.panc@gmail.com>
2020-11-12 19:18:46 +0000

    Update Gradle dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,9 +17,28 @@ repositories {
 dependencies {
     testImplementation(kotlin("test-js"))
     implementation("org.jetbrains.kotlinx:kotlinx-html-js:0.7.2")
-    implementation("org.jetbrains:kotlin-react:16.13.1-pre.110-kotlin-1.4.10")
-    implementation("org.jetbrains:kotlin-react-dom:16.13.1-pre.110-kotlin-1.4.10")
-    implementation("org.jetbrains:kotlin-styled:1.0.0-pre.110-kotlin-1.4.10")
+
+    implementation(kotlin("stdlib-js"))
+
+    //React, React DOM + Wrappers (chapter 3)
+    implementation("org.jetbrains:kotlin-react:16.13.1-pre.110-kotlin-1.4.0")
+    implementation("org.jetbrains:kotlin-react-dom:16.13.1-pre.110-kotlin-1.4.0")
+    implementation(npm("react", "16.13.1"))
+    implementation(npm("react-dom", "16.13.1"))
+
+    //Kotlin Styled (chapter 3)
+    implementation("org.jetbrains:kotlin-styled:1.0.0-pre.110-kotlin-1.4.0")
+    implementation(npm("styled-components", "~5.1.1"))
+    implementation(npm("inline-style-prefixer", "~6.0.0"))
+
+    //Video Player (chapter 7)
+    implementation(npm("react-player", "~2.6.0"))
+
+    //Share Buttons (chapter 7)
+    implementation(npm("react-share", "~4.2.1"))
+
+    //Coroutines (chapter 8)
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
 }
 kotlin {
     js {
